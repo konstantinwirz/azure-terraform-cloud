d8335f3e07ef1dde3f1001ee0f17cc5b8644902f
Konstantin Wirz <konstantinwirz@users.noreply.github.com>
2023-07-06 11:35:20 +0200

    Add k8s cluster (#4)

--- provider.tf
+++ provider.tf
@@ -1,4 +1,7 @@
 terraform {
+
+  required_version = "= 1.5.1"
+
   cloud {
     organization = "kwic"
     workspaces {
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "location" {
+  type = string
+  default = "northeurope"
+}
+
+variable "tags" {
+    type = map(string)
+}
